61e8695821eda6bbed0534e878a16966a5b307ed
IRUKA21C <vash21c@gmail.com>
2025-10-05 04:47:47 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# 기존 코드: st.title("My First App")
+# 수정 코드:
+st.title("🏫 대방중 1-6 알림장")
+st.write("1-6의 중요한 소식을 확인하세요!")
